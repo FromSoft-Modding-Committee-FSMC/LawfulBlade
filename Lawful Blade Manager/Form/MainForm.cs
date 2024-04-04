b570bcf61a9fa805e431a4172804e44a1715aa5a
TheStolenBattenberg <thestolenbattenberg@gmail.com>
2024-04-03 23:21:25 -0700

    Initial Push

---
+++ Lawful Blade Manager/Form/MainForm.cs
@@ -0,0 +1,15 @@
+namespace LawfulBladeManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
