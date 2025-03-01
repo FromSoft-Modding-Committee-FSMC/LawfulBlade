2731826bc96ec7dddc130ee2d726814442f00033
TheStolenBattenberg <thestolenbattenberg@gmail.com>
2025-02-28 21:46:33 -0800

    Final changes before pre-release testing

--- LawfulBlade/Dialog/PreferencesDialog.xaml.cs
+++ LawfulBlade/Dialog/PreferencesDialog.xaml.cs
@@ -22,6 +22,26 @@ namespace LawfulBlade.Dialog
         public PreferencesDialog()
         {
             InitializeComponent();
+
+            // Load current preferences into UI....
+            checkForUpdates.IsChecked = App.Preferences.AutoCheckForUpdates;
+            showDebugConsole.IsChecked = App.Preferences.ShowDebugConsole;
+            useLocaleEmulator.IsChecked = App.Preferences.UseLocaleEmulator;
+            localEmuPath.Text = App.Preferences.LocaleEmulatorPath;
         }
+
+        void OnClickConfirm(object sender, RoutedEventArgs e)
+        {
+            App.Preferences.AutoCheckForUpdates = checkForUpdates.IsChecked ?? false;
+            App.Preferences.ShowDebugConsole = showDebugConsole.IsChecked ?? false;
+            App.Preferences.UseLocaleEmulator = useLocaleEmulator.IsChecked ?? false;
+            App.Preferences.LocaleEmulatorPath = localEmuPath.Text ?? string.Empty;
+            App.Preferences.Save();
+
+            Close();
+        }
+
+        void OnClickCancel(object sender, RoutedEventArgs e) =>
+            Close();
     }
 }
