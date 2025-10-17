2f9edeeb6cc9231f1f773264d4c0dee457823128
TheStolenBattenberg <thestolenbattenberg@gmail.com>
2025-10-17 11:21:11 -0700

    Refactoring the updater for easier reading, and code safety

---
+++ LawfulBladeSDK/Extensions/LawfulBladeStringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawfulBladeSDK.Extensions
+{
+    /// <summary>
+    /// Extensions for the string data type.
+    /// </summary>
+    public static class LawfulBladeStringExtensions
+    {
+        /// <summary>
+        /// Wraps a string with ansi colour codes
+        /// </summary>
+        public static string Colourise(this string text, uint colour) =>
+            $"\u001b[38;2;{(colour >> 16) & 0xFF};{(colour >> 8) & 0xFF};{(colour >> 0) & 0xFF}m{text}\u001b[0m";
+    }
+}
