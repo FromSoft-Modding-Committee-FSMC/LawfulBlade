4304b265f7bce631aa35677e980c99faf7ee3710
TheStolenBattenberg <thestolenbattenberg@gmail.com>
2025-10-20 15:51:43 -0700

    Added Extra/CommandLineSleuth
    Used for finding out secret arguments sent to various SoM tools

---
+++ Extra/CommandLineSleuth/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandLineSleuth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write out each argument, then pause
+            for (int i = 0; i < args.Length; ++i)
+                Console.WriteLine($"Argument #{1 + i}: {args[i]}");
+
+            Console.ReadKey();
+        }
+    }
+}
